9908e82931cc6c14d460ade52a241306b7e02c39
Nikola Krstajic <nikolakrstajic@yahoo.com>
2024-12-26 00:04:23 +0100

    Dodat window za evente

--- src/base/Mechanics/event.cpp
+++ src/base/Mechanics/event.cpp
@@ -1,8 +1,6 @@
 #include "event.h"
-#include <sstream>
-#include <cmath>
-#include <iostream>
-// Constructor implementation
+
+
 Event::Event(EventType type, const std::string& outcome,
              int outcomeGain, const std::vector<MapLayer*>& territories,
              Army *army, const std::string& date)
@@ -24,32 +22,85 @@ Army* Event::getArmy() const {return army;}
 
 std::string Event::getDate() const {return date;}
 
+QString Event::getPicturePath() const {return picture_path;}
 
-double Event::calculateEventPercentage(const std::string &currentDate ) const{
-    int eventYear,eventMonth;
-
-    std::stringstream ssYear(date.substr(date.rfind('-') + 1 ));
-    std::stringstream ssMonth(date.substr(date.find('-') + 1, 2));
-    ssYear >> eventYear;
-    ssMonth >> eventMonth;
-
-    int currentYear, currentMonth;
-
-    std::stringstream ssCurrentYear(currentDate.substr(currentDate.rfind('-') + 1));
-    std::stringstream ssCurrentMonth(currentDate.substr(currentDate.find('-') + 1, 2));
+void Event::showEventWindow() {
 
 
-    int eventQuarter = (eventMonth - 1)/3+1;
-    int currentQuarter = (currentMonth -1 )/3+1;
+    infoWindow = new QWidget;
+    infoWindow->setWindowTitle("Event Details");
+    infoWindow->setMinimumSize(600,400);
 
-    int quartersSinceEvent = (currentYear - eventYear)*4+(currentQuarter - eventQuarter);
+    infoWindow->setStyleSheet(
+        "background-color: #f0e6d2;"
+        "border: 4px solid #4e3629;"
+        "border-radius: 15px;"
+        );
 
-    double eventPercentage = std::exp(-quartersSinceEvent/100.0) * 100.0;
-    return eventPercentage;
+    QVBoxLayout * layout = new QVBoxLayout(infoWindow);
+
+
+    QString eventType;
+    switch(type){
+        case EventType::BATTLE:
+            eventType = "Battle Event";
+            break;
+        case EventType::PROGRESS:
+            eventType = "Progress Event";
+            break;
+        case EventType:: UPRISING:
+            eventType = "Uprising Event";
+            break;
+        case EventType::ECONOMIC_EVENT:
+            eventType = "Economic event";
+            break;
+        case EventType::NATURAL_DISASTER:
+            eventType = "Natural Disaster";
+            break;
+    }
+
+        typeLabel = new QLabel("<h1 style='color: #d7a13d; font-family: \"SimSun\", serif; font-size: 26px;'>"
+    "Karadjordje zauzima Cetinje</h1>", infoWindow);
+    typeLabel->setAlignment(Qt::AlignCenter);
+    typeLabel->setStyleSheet("border: none");
+    layout->addWidget(typeLabel);
+
+    QLabel *imageLabel = new QLabel(infoWindow);
+    QPixmap label_image(picture_path);
+    label_image = label_image.scaled(infoWindow->width(),100,Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+    imageLabel->setPixmap(label_image);
+    imageLabel->setAlignment(Qt::AlignCenter);
+    imageLabel->setStyleSheet("margin-bottom: 15px");
+    layout->addWidget(imageLabel);
+
+    outcomeLabel = new QLabel(QString("<p style='color: #6b3f3d; font-size: 16px;'>%1</p>")
+                                  .arg(QString::fromStdString(outcome)), infoWindow);
+
+    layout->addWidget(outcomeLabel);
+
+    QPushButton *closeButton = new QPushButton("X",infoWindow);
+    layout->addWidget(closeButton);
+    closeButton->setFixedSize(70,50);
+    closeButton->setStyleSheet(        "background-color: #ff6347;"
+                               "border: 1px solid #8b4513;"
+                               "border-radius: 5px;"
+                               "text-align: center;"
+                               "font-size: 14px; padding: 8px;");
+
+
+    gainLabel = new QLabel(QString("<p style='color: #006400;'>Outcome Gain: %1</p>").arg(outcomeGain), infoWindow);
+    gainLabel->setStyleSheet("boder : none;");
+    layout->addWidget(gainLabel);
+
+    territoryCountLabel = new QLabel(QString("<p style='color: #4b0082;'>Territories Affected: %1</p>").arg(territories.size()), infoWindow);
+    layout->addWidget(territoryCountLabel);
+
+    QObject::connect(closeButton, &QPushButton::clicked,infoWindow,&QWidget::close);
+
+    infoWindow->show();
 
 }
 
-// Print event details
 void Event::printEventDetails() const {
     std::cout << "Event Description: " << description << "\n"
               << "Type: " << static_cast<int>(type) << "\n"
--- src/base/Mechanics/event.h
+++ src/base/Mechanics/event.h
@@ -5,8 +5,18 @@
 #include "../../Interface/maplayer.h"
 #include "../Entities/Army.h"
 #include <vector>
-
-
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <qpushbutton.h>
+#include <QObject>
+#include <QDialog>
+#include <QPushButton>
+#include <QString>
+#include <sstream>
+#include <cmath>
+#include <iostream>
+#include <QApplication>
 
 enum class EventType{
     BATTLE,
@@ -16,36 +26,48 @@ enum class EventType{
     NATURAL_DISASTER
 };
 
-class Event{
+class Event {
 
-    private:
-        EventType type;
-        std::string description;
-        std::string outcome;
-        int outcomeGain;
-        std::vector<MapLayer*> territories;
-        Army *army;
-        std::string date;
+public:
+    Event();
 
-    public:
-        Event(EventType type, const std::string& outcome,
-                int outcomeGain, const std::vector<MapLayer*>& territories,
-              Army* army, const std::string& date);
+    Event(EventType type, const std::string& outcome,
+          int outcomeGain, const std::vector<MapLayer*>& territories,
+          Army* army, const std::string& date);
 
-        EventType getType() const;
-
-        std::string getDescription() const;
-        std::string getOutcome() const;
-        int getOutcomeGain() const;
-        const std::vector<MapLayer*>& getTerritories() const;
-        Army* getArmy() const;
-        std::string getDate() const;
+    EventType getType() const;
 
 
-        double calculateEventPercentage(const std::string& currentDate) const;
-        void printEventDetails() const;
-        EventType stringToEventType(const std::string& str);
 
+    std::string getDescription() const;
+    QString getPicturePath() const;
+    std::string getOutcome() const;
+    int getOutcomeGain() const;
+    const std::vector<MapLayer*>& getTerritories() const;
+    Army* getArmy() const;
+    std::string getDate() const;
+    void showEventWindow();
+
+    void printEventDetails() const;
+    EventType stringToEventType(const std::string str);
+
+
+
+private:
+    EventType type;
+    std::string description;
+    QString picture_path;
+    std::string outcome;
+    int outcomeGain;
+    std::vector<MapLayer*> territories;
+    Army *army;
+    QWidget *infoWindow;
+    QLabel *typeLabel;
+    QLabel *outcomeLabel;
+    QLabel *gainLabel;
+    QLabel *territoryCountLabel;
+    QLabel *dateLabel;
+    std::string date;
 };
 
 
